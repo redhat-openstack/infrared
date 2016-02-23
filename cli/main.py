349c451fafae4f2225d42bc2d6defb110e6519ae
Ariel Opincaru <aopincar@redhat.com>
2016-02-23 16:29:28 +0200

    Adds auto playbook execution for virsh provisioner

--- cli/main.py
+++ cli/main.py
@@ -18,7 +18,7 @@ LOG = logger.LOG
 CONF = conf.config
 
 NON_SETTINGS_OPTIONS = ['command0', 'verbose', 'extra-vars', 'output-file',
-                        'input-files']
+                        'input-files', 'dry-run']
 
 
 def main():
@@ -28,6 +28,8 @@ def main():
 
     cmd = clg.CommandLine(yaml.load(open(CONF.get('DEFAULTS', 'SPEC_FILE'))))
     args = cmd.parse()
+
+    verbose = int(args.verbose)
 
     if args.verbose == 0:
         args.verbose = logging.WARNING
@@ -42,6 +44,9 @@ def main():
 
     for input_file in args['input-files'] or []:
         settings_files.append(utils.normalize_file(input_file))
+
+    settings_files.append(os.path.join(provision_dir,
+                                       '../' + args['command0'] + '.yml'))
 
     for key, val in vars(args).iteritems():
         if val is not None and key not in NON_SETTINGS_OPTIONS:
@@ -65,10 +70,40 @@ def main():
                             default_flow_style=False)
 
     if args['output-file']:
-        with open(args.output_file, 'w') as output_file:
+        with open(args['output-file'], 'w') as output_file:
             output_file.write(output)
     else:
         print output
+
+    # playbook execution stage
+    if not args['dry-run']:
+        args_list = ["execute"]
+        if verbose:
+            args_list.append('-%s' % ('v' * verbose))
+        args_list.append('--inventory=local_hosts')
+        args_list.append('--provision')
+        args_list.append('--collect-logs')
+        if args['output-file']:
+            LOG.debug('Using the newly created settings file: "%s"'
+                      % args['output-file'])
+            args_list.append('--settings=%s' % args['output-file'])
+        else:
+            with open(conf.TMP_OUTPUT_FILE, 'w') as output_file:
+                output_file.write(output)
+            LOG.debug('Temporary settings file "%s" has been created for '
+                      'execution purpose only.' % conf.TMP_OUTPUT_FILE)
+            args_list.append('--settings=%s' % conf.TMP_OUTPUT_FILE)
+
+        parser = cli.parse.create_parser(list())
+        execute_args = parser.parse_args(args_list)
+
+        LOG.debug("execute parser args: %s" % execute_args)
+        execute_args.func(execute_args)
+
+        if not args['output-file'] and args.which != 'execute':
+            LOG.debug('Temporary settings file "%s" has been deleted.'
+                      % conf.TMP_OUTPUT_FILE)
+            os.remove(conf.TMP_OUTPUT_FILE)
 
 
 if __name__ == '__main__':
