0c0a5d681133b325da03d949c71fa04bb72a0c1f
obaranov <obaranov@redhat.com>
2016-02-29 16:53:03 +0200

    Added IRApplication class (for provisioner)

--- cli/provision.py
+++ cli/provision.py
@@ -2,6 +2,9 @@
 
 import os
 
+from cli import logger  # logger creation is first thing to be done
+import cli.execute
+import cli.yamls
 import yaml
 
 from cli import logger  # logger creation is first thing to be done
@@ -16,72 +19,141 @@ CONF = conf.config
 NON_SETTINGS_OPTIONS = ['command0', 'verbose', 'extra-vars', 'output-file',
                         'input-files', 'dry-run', 'cleanup', 'inventory']
 
-
-def set_logger_verbosity(level):
+class IRFactory(object):
     """
-    Set the logger verbosity level
-
-    :param level: verbosity level (int)
+    Creates and configures tha IR applications.
     """
     from logging import WARNING, INFO, DEBUG
     LOG.setLevel((WARNING, INFO)[level] if level < 2 else DEBUG)
 
+    NON_SETTINGS_OPTIONS = ['command0', 'verbose', 'extra-vars', 'output-file',
+                            'input-files', 'dry-run', 'cleanup', 'inventory']
 
-def main():
-    spec_manager = conf.SpecManager(CONF)
-    args = spec_manager.parse_args("provisioner")
+    @classmethod
+    def create(cls, app_name, config):
+        """
+        Create the application object
+        by module name and provided configuration.
+        """
+        if app_name in ["provisioner", ]:
+            args = conf.SpecManager.parse_args(app_name, config)
+            settings_files = cls.configure(app_name, config, args)
+            app_instance = IRApplication(app_name, args, settings_files)
 
-    settings_files = []
-    settings_dir = utils.validate_settings_dir(
-        CONF.get('defaults', 'settings'))
+        else:
+            raise Exception("Application is not supported: '{}'".format(app_name))
+        return app_instance
 
-    set_logger_verbosity(args.verbose)
+    @classmethod
+    def configure(cls, app_name, config, args):
+        cls._configure_log(args)
+        settings_files = cls._collect_settings_files(app_name, config, args)
+        LOG.debug("All settings files to be loaded:\n%s" % settings_files)
+        return settings_files
 
-    provision_dir = os.path.join(settings_dir, 'provisioner', args['command0'])
+    @classmethod
+    def _configure_log(cls, args):
+        """
+        Perfroms the logging module configuration.
+        :return:
+        """
+        from logging import WARNING, INFO, DEBUG
+        LOG.setLevel((WARNING, INFO)[args.verbose]
+                     if args.verbose < 2 else DEBUG)
 
-    for input_file in args['input-files'] or []:
-        settings_files.append(utils.normalize_file(input_file))
+    @classmethod
+    def _collect_settings_files(cls, app_name, config, args):
+        """
+        Collects all the settings files for given module and sub-command
+        """
+        settings_files = []
+        settings_dir = utils.validate_settings_dir(
+            config.get('defaults', 'settings'))
+        module_dir = os.path.join(
+            settings_dir,
+            app_name,
+            args.command0)
 
-    settings_files.append(os.path.join(provision_dir,
-                                       args['command0'] + '.yml'))
+        # first take all the files from the input-files args
+        for input_file in args['input-files'] or []:
+            settings_files.append(utils.normalize_file(input_file))
 
-    for key, val in vars(args).iteritems():
-        if val is not None and key not in NON_SETTINGS_OPTIONS:
-            settings_file = os.path.join(provision_dir, key, val + '.yml')
-            LOG.debug('Searching settings file for the "%s" key...' % key)
-            if not os.path.isfile(settings_file):
-                settings_file = utils.normalize_file(val)
-            settings_files.append(settings_file)
-            LOG.debug('"%s" was added to settings files list as an argument '
-                      'for "%s" key' % (settings_file, key))
+        # get the sub-command yml file
+        settings_files.append(os.path.join(
+            module_dir,
+            args.command0 + '.yml'))
 
-    LOG.debug("All settings files to be loaded:\n%s" % settings_files)
+        # load directly from args
+        for key, val in vars(args).iteritems():
+            if val is not None and key not in cls.NON_SETTINGS_OPTIONS:
+                settings_file = os.path.join(module_dir, key, val + '.yml')
+                LOG.debug('Searching settings file for the "%s" key...' % key)
+                if not os.path.isfile(settings_file):
+                    settings_file = utils.normalize_file(val)
+                settings_files.append(settings_file)
+                LOG.debug('"%s" was added to settings '
+                          'files list as an argument '
+                          'for "%s" key' % (settings_file, key))
 
-    cli.yamls.Lookup.settings = utils.generate_settings(settings_files,
-                                                        args['extra-vars'])
+        return settings_files
 
     cli.yamls.Lookup.in_string_lookup()
 
-    LOG.debug("Dumping settings...")
+class IRApplication(object):
+    """
+    Hold the default application workflow logic.
+    """
+    def __init__(self, name, args, settings_files):
+        self.name = name
+        self.args = args
+        self.settings_files = settings_files
 
-    output = yaml.safe_dump(cli.yamls.Lookup.settings,
-                            default_flow_style=False)
+    def run(self):
+        """
+        :return: Runs the application
+        """
+        settings = self.lookup(self.settings_files)
+        self.dump_settings(settings)
+        self.execute(settings)
 
-    if args['output-file']:
-        with open(args['output-file'], 'w') as output_file:
-            output_file.write(output)
-    else:
-        print output
+    def lookup(self, settings_files):
+        """
+        Replaces a setting values with !lookup
+        in the setting files by values from
+        other settings files.
+        """
+        cli.yamls.Lookup.settings = utils.generate_settings(
+            settings_files,
+            self.args['extra-vars'])
+        cli.yamls.Lookup.in_string_lookup()
 
-    # playbook execution stage
-    if not args['dry-run']:
-        vars(args)['settings'] = yaml.load(yaml.safe_dump(
-            cli.yamls.Lookup.settings, default_flow_style=False))
-        if not args['cleanup']:
-            vars(args)['provision'] = True
+        return cli.yamls.Lookup.settings
 
-        cli.execute.ansible_wrapper(args)
+    def dump_settings(self, settings):
+        LOG.debug("Dumping settings...")
+        output = yaml.safe_dump(settings,
+                                default_flow_style=False)
+        if self.args['output-file']:
+            with open(self.args['output-file'], 'w') as output_file:
+                output_file.write(output)
+        else:
+            print output
 
+    def execute(self, settings):
+        """
+        Executes a playbook.
+        """
+        if not self.args['dry-run']:
+            vars(self.args)['settings'] = settings
+            if not self.args['cleanup']:
+                vars(self.args)['provision'] = True
+
+        cli.execute.ansible_wrapper(self.args)
+
+
+def main():
+    app = IRFactory.create('provisioner', CONF)
+    app.run()
 
 if __name__ == '__main__':
     main()
