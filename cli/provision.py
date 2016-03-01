b90a4bd5edca6c960089d61948ed0e963426f81d
obaranov <obaranov@redhat.com>
2016-03-01 19:35:00 +0200

    Fixed merge issues.

--- cli/provision.py
+++ cli/provision.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 import os
+from logging import WARNING, INFO, DEBUG
+from cli.install import set_network_template as set_network
 
 import yaml
 
 from cli import logger  # logger creation is first thing to be done
 from cli import conf
 from cli import utils
+from cli import exceptions
 import cli.yamls
 import cli.execute
 
@@ -94,7 +97,14 @@ class IRSubCommand(object):
             self.settings_dir,
             self.name + '.yml'))
 
+        settings_files.extend(self._load_yaml_files())
+
+        LOG.debug("All settings files to be loaded:\n%s" % settings_files)
+        return settings_files
+
+    def _load_yaml_files(self):
         # load directly from args
+        settings_files = []
         for key, val in vars(self.args).iteritems():
             if val is not None and key not in NON_SETTINGS_OPTIONS:
                 settings_file = os.path.join(
@@ -106,9 +116,44 @@ class IRSubCommand(object):
                 LOG.debug('"%s" was added to settings '
                           'files list as an argument '
                           'for "%s" key' % (settings_file, key))
-
-        LOG.debug("All settings files to be loaded:\n%s" % settings_files)
         return settings_files
+
+    def get_settings_dict(self):
+        return {}
+
+
+class VirshCommand(IRSubCommand):
+
+    def get_settings_dict(self):
+
+        # todo(obaranov) this is virsh specific
+        # rework that and make this part of lookup or something.
+        image = dict(
+            name=self.args['image-file'],
+            base_url=self.args['image-server']
+        )
+        host = dict(
+            ssh_host=self.args['host'],
+            ssh_user=self.args['ssh-user'],
+            ssh_key_file=self.args['ssh-key']
+        )
+
+        settings_dict = utils.dict_merge(
+            {'provisioner': {'image': image}},
+            {'provisioner': {'hosts': {'host1': host}}})
+
+        # load network and image settings
+        for arg_dir in ('network', 'topology'):
+            with open(set_network(self.args[arg_dir], os.path.join(
+                    self.settings_dir, arg_dir))) as settings_file:
+                settings = yaml.load(settings_file)
+            utils.dict_merge(settings_dict, settings)
+
+        return settings_dict
+
+    def _load_yaml_files(self):
+        # do not load additional yaml files.
+        return []
 
 
 class IRApplication(object):
@@ -119,17 +164,25 @@ class IRApplication(object):
         self.name = name
         self.args = args
         self.settings_dir = settings_dir
-        self.sub_command = IRSubCommand.create(name, settings_dir, args)
+
+        # todo(obaranov) replace with subcommand factory
+        self.sub_command = VirshCommand.create(name, settings_dir, args)
 
     def run(self):
         """
         :return: Runs the application
         """
-        settings = self.lookup(self.sub_command.get_settings_files())
+        settings = self.collect_settings()
         self.dump_settings(settings)
         self.execute(settings)
 
-    def lookup(self, settings_files):
+    def collect_settings(self):
+        settings_files = self.sub_command.get_settings_files()
+        settings_dict = self.sub_command.get_settings_dict()
+
+        return self.lookup(settings_files, settings_dict)
+
+    def lookup(self, settings_files, settings_dict):
         """
         Replaces a setting values with !lookup
         in the setting files by values from
@@ -138,6 +191,8 @@ class IRApplication(object):
         cli.yamls.Lookup.settings = utils.generate_settings(
             settings_files,
             self.args['extra-vars'])
+
+        cli.yamls.Lookup.settings.merge(settings_dict)
         cli.yamls.Lookup.in_string_lookup()
 
         return cli.yamls.Lookup.settings
@@ -146,8 +201,10 @@ class IRApplication(object):
         LOG.debug("Dumping settings...")
         output = yaml.safe_dump(settings,
                                 default_flow_style=False)
-        if self.args['output-file']:
-            with open(self.args['output-file'], 'w') as output_file:
+        dump_file = self.args['output-file']
+        if dump_file:
+            LOG.debug("Dump file: {}".format(dump_file))
+            with open(dump_file, 'w') as output_file:
                 output_file.write(output)
         else:
             print output
@@ -165,7 +222,6 @@ class IRApplication(object):
 
             cli.execute.ansible_wrapper(self.args)
 
-        cli.execute.ansible_wrapper(args)
 
 def main():
     app = IRFactory.create('provisioner', CONF)
