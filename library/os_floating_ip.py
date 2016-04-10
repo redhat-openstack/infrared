a2722ba5c1cb23ed4a549c4f61fb9617ba14b394
Arie Bregman <abregman@redhat.com>
2016-04-10 13:38:27 +0300

    Fix Openstack provisioner
    'ipaddress' is used in os_floating_ip, but the module is never
    imported. This change fix that + adding 'ipaddress' to requirements
    file.
    
    Also, added 'wait=yes" to floating_ip creation. Without a user might
    hit race condition.

--- library/os_floating_ip.py
+++ library/os_floating_ip.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
+import ipaddress
+
 try:
     import shade
     from shade import meta
