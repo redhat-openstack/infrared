2874a072fc1a6ec6b3da818c1910424168837a32
Arie Bregman <abregman@redhat.com>
2016-04-10 09:52:06 +0300

    Fix os_floating_ip module
    'network' parameter is not used at all in the 'os_floating_ip'
    module.
    
    This fix it so the creation of the floating ip would be based
    on the chosen pool/network.
    
    This also adds 'python-novaclient<3' to the requirements. Using
    higher version will cause the os_floating_ip module to fail.

--- library/os_floating_ip.py
+++ library/os_floating_ip.py
@@ -182,7 +182,8 @@ def main():
 
             cloud.add_ips_to_server(
                 server=server, ips=floating_ip_address, reuse=reuse,
-                fixed_address=fixed_address, wait=wait, timeout=timeout)
+                ip_pool=network, fixed_address=fixed_address, wait=wait,
+                timeout=timeout)
             fip_address = cloud.get_server_public_ip(server)
             # Update the floating IP status
             f_ip = _get_floating_ip(cloud, fip_address)
