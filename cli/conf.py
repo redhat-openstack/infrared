caf35166594c6b3395bdd713a14e82a440a4c4fb
obaranov <obaranov@redhat.com>
2016-04-21 14:16:28 +0300

    Fix auto cfg by adding provisioner and installer section

--- cli/conf.py
+++ cli/conf.py
@@ -7,12 +7,25 @@ from cli import utils
 from cli import logger
 
 LOG = logger.LOG
-DEFAULT_CONF_DIRS = dict(
-    settings='settings',
-    modules='library',
-    roles='roles',
-    playbooks='playbooks'
-)
+
+DEFAULT_SECTIONS = {
+    'defaults': dict(
+        settings='settings',
+        modules='library',
+        roles='roles',
+        playbooks='playbooks'
+    ),
+
+    'provisioner':  dict(
+        main_playbook='provision.yml',
+        cleanup_playbook='cleanup.yml'
+    ),
+
+    'installer': dict(
+        main_playbook='install.yml',
+        cleanup_playbook='cleanup.yml'
+    )
+}
 
 
 def load_config_file():
@@ -39,9 +52,11 @@ def load_config_file():
         LOG.warning("Configuration file not found, using InfraRed project dir")
         project_dir = os.path.dirname(os.path.dirname(__file__))
 
-        _config.add_section('defaults')
-        for option, value in DEFAULT_CONF_DIRS.iteritems():
-            _config.set('defaults', option, os.path.join(project_dir, value))
+        for section_name, section_data in DEFAULT_SECTIONS.items():
+            _config.add_section(section_name)
+            for option, value in section_data.items():
+                _config.set(section_name, option, os.path.join(
+                    project_dir, value))
 
     # Validates settings dir exists
     settings_dir = _config.get('defaults', 'settings')
