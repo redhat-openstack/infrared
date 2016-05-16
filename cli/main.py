2e2c4a94b0d9b8ce86579c0cfaf0afe923a5aa51
obaranov <obaranov@redhat.com>
2016-05-16 20:12:29 +0300

    CLG module rework
    This modules removes the clg dependency and moves
    all the spec logic to one module: ngclg.
    
    Major changes includes:
     * restructured spec files. All the options are now
     stored in groups. Global groups can be defined and shared
     * created base spec files for all the apps (installer, tester, etc)
     * all the args are now devided to control and nested.  Control
     arguments (e.g. dry-run, ouput, cleanup) are used to control
     InfraRed flows. They are not inserted into the ansible settings files
     Nested arguments are used by the ansible playbooks.
     * added required_when keyword for option to indicate condition when
      certain argument can be required
     * added silent keyword to indicated when required argument can be omitted (
     for example for cleanup commands)
     * -d, -vvvv argguments are moved from ir-* to the subcommands (virsh -d -vvvv)
     * fixed issue with the required arguments indication in the help output
     * updated unit tests
     * renamed spec file clg to keep change history
    
    Change-Id: Ifc51d8afe5d0b500379b29c7e5a83342aec6a7fb

--- cli/main.py
+++ cli/main.py
@@ -4,8 +4,8 @@ import inspect
 import os
 
 import yaml
-from cli import logger  # logger should bne imported first
-from cli import execute, conf, spec, utils, exceptions, yamls
+from cli import logger  # logger should be imported first
+from cli import execute, conf, clg, utils, exceptions, yamls
 
 LOG = logger.LOG
 
@@ -27,10 +27,9 @@ def get_arguments_dict(spec_args):
     :return: dict
     """
     settings_dict = {}
-    for _name, argument in spec_args.iteritems():
-        if isinstance(argument, spec.ValueArgument):
-            utils.dict_insert(settings_dict, argument.value,
-                              *argument.arg_name.split("-"))
+    for _name, argument in spec_args.items():
+        utils.dict_insert(settings_dict, argument,
+                          *_name.split("-"))
 
     return settings_dict
 
@@ -41,14 +40,13 @@ class IRFactory(object):
     """
 
     @classmethod
-    def create(cls, spec_name, config, args=None):
+    def create(cls, spec_name, config):
         """
         Create the spec object
         by module name and provided configuration.
 
         :param spec_name: str. the specification name
         :param config: dict. configuration details
-        :param args: list. If given parse it instead of stdin input.
         """
         settings_dirs = config.get_settings_dirs()
         supported_specs = cls.get_supported_specs(config)
@@ -56,16 +54,22 @@ class IRFactory(object):
         if spec_name not in supported_specs:
             raise exceptions.IRUnknownSpecException(spec_name)
         else:
-            app_settings_dirs = config.build_app_settings_dirs(spec_name)
-            spec_args = spec.parse_args(settings_dirs, app_settings_dirs, args)
-            cls.configure_environment(spec_args)
+            spec_app = clg.Spec.from_folder(settings_dirs, spec_name)
+            spec_args = spec_app.parse_args()
 
-            if spec_args.get('generate-conf-file', None):
-                LOG.debug('Conf file "{}" has been generated. Exiting'.format(
-                    spec_args['generate-conf-file']))
+            if spec_args is None:
                 spec_instance = None
             else:
-                spec_instance = IRSpec(config, spec_name, spec_args)
+                nested_args = spec_args[0]
+                control_args = spec_args[1]
+                unknown_args = spec_args[2]
+                cls.configure_environment(control_args)
+                spec_instance = IRSpec(
+                    config,
+                    spec_name,
+                    control_args,
+                    nested_args,
+                    unknown_args)
 
         return spec_instance
 
@@ -79,44 +83,30 @@ class IRFactory(object):
         return supported_specs
 
     @classmethod
-    def configure_environment(cls, args):
+    def configure_environment(cls, control_args):
         """
         Performs the environment configuration.
         :param args:
         :return:
         """
-        if args.get('debug', None):
+        if control_args.get('debug', None):
             LOG.setLevel(logging.DEBUG)
             # todo(yfried): load exception hook now and not at init.
 
 
-class IRSubCommand(object):
+class IRSpec(object):
     """
-    Represents a command (virsh, ospd, etc)
-    for the spec.
+    Hold the default spec workflow logic.
     """
 
-    def __init__(self, name, args, settings_dirs):
+    def __init__(self, config, name, control_args, nested_args, unknown_args):
+        self.unknown_args = unknown_args
+        self.config = config
         self.name = name
-        self.args = args
-        self.settings_dirs = settings_dirs
-
-    @classmethod
-    def create(cls, spec_name, settings_dirs, args):
-        """
-        Constructs the sub-command.
-        :param spec_name:  tha spec name
-        :param settings_dirs:  the default spec settings dirs
-        :param args: the spec args
-        :return: the IRSubCommand instance.
-        """
-        if args:
-            settings_dirs = [os.path.join(settings_dir, spec_name)
-                             for settings_dir in settings_dirs]
-        if "command0" in args:
-            settings_dirs = [os.path.join(settings_dir, args['command0'])
-                             for settings_dir in settings_dirs]
-        return cls(args['command0'], args, settings_dirs)
+        self.nested_args = nested_args
+        self.control_args = control_args
+        self.spec_config = config.get_spec_config(name)
+        self.settings_dirs = config.get_settings_dirs()
 
     def get_settings_files(self):
         """
@@ -125,33 +115,22 @@ class IRSubCommand(object):
         settings_files = []
 
         # first take all input files from args
-        for input_file in self.args.get('input', []) or []:
+        for input_file in self.control_args.get('input', []) or []:
             settings_files.append(utils.normalize_file(input_file))
 
         # get the sub-command yml file
+        cmd_name = self.control_args['command0']
         for settings_dir in self.settings_dirs:
             path = os.path.join(
                 settings_dir,
-                self.name + '.yml')
+                self.name,
+                cmd_name,
+                cmd_name + '.yml')
             if os.path.exists(path):
                 settings_files.append(path)
 
         LOG.debug("All settings files to be loaded:\n%s" % settings_files)
         return settings_files
-
-
-class IRSpec(object):
-    """
-    Hold the default spec workflow logic.
-    """
-
-    def __init__(self, config, name, args):
-        self.config = config
-        self.name = name
-        self.args = args
-        self.spec_config = config.get_spec_config(name)
-        self.settings_dirs = config.get_settings_dirs()
-        self.sub_command = IRSubCommand.create(name, self.settings_dirs, args)
 
     def run(self):
         """
@@ -160,29 +139,29 @@ class IRSpec(object):
         settings = self.collect_settings()
         self.dump_settings(settings)
 
-        if not self.args.get('dry-run'):
-            self.args['settings'] = yaml.load(yaml.safe_dump(
+        if not self.control_args.get('dry-run'):
+            playbook_settings = yaml.load(yaml.safe_dump(
                 settings,
                 default_flow_style=False))
 
-            if self.args.get('cleanup', None):
+            if self.control_args.get('cleanup', None):
                 execute.ansible_playbook(
                     self.config,
                     self.spec_config['cleanup_playbook'],
-                    verbose=self.args['verbose'],
-                    settings=self.args['settings'],
-                    inventory=self.args['inventory'])
+                    verbose=self.control_args['verbose'],
+                    settings=playbook_settings,
+                    inventory=self.control_args['inventory'])
             else:
                 execute.ansible_playbook(
                     self.config,
                     self.spec_config['main_playbook'],
-                    verbose=self.args.get('verbose', None),
-                    settings=self.args['settings'],
-                    inventory=self.args.get('inventory', None))
+                    verbose=self.control_args.get('verbose', None),
+                    settings=playbook_settings,
+                    inventory=self.control_args.get('inventory', None))
 
     def collect_settings(self):
-        settings_files = self.sub_command.get_settings_files()
-        arguments_dict = {self.name: get_arguments_dict(self.args)}
+        settings_files = self.get_settings_files()
+        arguments_dict = {self.name: get_arguments_dict(self.nested_args)}
 
         # todo(yfried): fix after lookup refactor
         # utils.dict_merge(settings_files, arguments_dict)
@@ -199,7 +178,9 @@ class IRSpec(object):
         """
         all_settings = utils.load_settings_files(settings_files)
         utils.dict_merge(all_settings, settings_dict)
-        utils.merge_extra_vars(all_settings, self.args.get('extra-vars', None))
+        utils.merge_extra_vars(
+            all_settings,
+            self.control_args.get('extra-vars', None))
         yamls.replace_lookup(all_settings)
 
         return all_settings
@@ -208,7 +189,7 @@ class IRSpec(object):
         LOG.debug("Dumping settings...")
         output = yaml.safe_dump(settings,
                                 default_flow_style=False)
-        dump_file = self.args.get('output', None)
+        dump_file = self.control_args.get('output', None)
         if dump_file:
             LOG.debug("Dump file: {}".format(dump_file))
             with open(dump_file, 'w') as output_file:
