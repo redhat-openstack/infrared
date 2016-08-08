51899c32909aaf6dfcc0166edfcdb7468df33967
Pavel Sedlák <psedlak@redhat.com>
2016-08-08 14:17:47 +0200

    tox: some colors to separate pep8/py/lint outputs
    Change-Id: Ie17abdbf729c442b6903f6eba8e85bdd8f0a4a44

--- tox-gate.sh
+++ tox-gate.sh
@@ -12,17 +12,21 @@ run_tox() {
 }
 
 verify_tox() {
+    echo -e "\033[01;33m_________ output of $1: _________\033[0m"
     cat .tox_$1_out
-    echo "======"
     if ! tail -n2 .tox_$1_out | grep -q "$1: commands succeeded"; then
         FAILED="${FAILED} $1"
+        echo -e "\033[01;31m====== ^^ enf of failed $1 ^^ ======\033[0m"
+    else
+        echo -e "\033[01;33m====== ^^ end of $1 ^^ ======\033[0m"
     fi
 }
 
 for tEnv in ${RUN_ENVS}; do run_tox ${tEnv}; done
 wait
+set +x
 for tEnv in ${RUN_ENVS}; do verify_tox ${tEnv} || exit 1; done
 if [[ ! -z "$FAILED" ]]; then
-    echo "==== Failed: $FAILED ====" >&2
+    echo "\033[01;31m==== Failed: $FAILED ====\033[0m" >&2
     exit 1
 fi
