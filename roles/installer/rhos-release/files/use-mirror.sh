95d183997040d97a46b04129e8e0f6b79be81b72
Pavel Sedlák <psedlak@redhat.com>
2016-08-25 13:54:20 +0200

    mirror: do not use it for pip yet
    it appears to cause some (not clear yet) issue
    for ansible's pip task, virtualenv path being ignored then
    
    so do not enable pip mirror usage until we know more
    
    Change-Id: Ife0e3467a00fe095f90a50fc2675ea939b10fc83

--- roles/installer/rhos-release/files/use-mirror.sh
+++ roles/installer/rhos-release/files/use-mirror.sh
@@ -27,30 +27,33 @@ sed '/.* download\.lab.*redhat\.com.*/d' -i /etc/hosts
 echo "$mirror_ip  $mirror $original_download_srv $original_download_srv_alts" >> /etc/hosts
 echo "In case you want to disable mirror usage, also remove its entry from /etc/hosts" >> mirror-readme
 
+# FIXME(psedlak):
+# pypi mirror usage disabled atm, seems to cause issue with ansible's 'pip: virtualenv='
+# not using specified venv at the end
 
-# patch pip/easy_install config
-
-pip_index_url="index_url = http://${mirror}/root/pypi/+simple"
-pip_trusted_host="trusted-host = ${mirror}"
-pip_use_wheel="True"
-
-for HDIR in /root $(find /home -maxdepth 1 -type d|grep -v '/home$'); do
-    [[ -d $HDIR ]] || continue
-
-cat > $HDIR/.pydistutils.cfg <<EOF
-[easy_install]
-$pip_index_url
-EOF
-
-mkdir -p $HDIR/.pip;
-cat > $HDIR/.pip/pip.conf <<EOF
-[global]
-$pip_index_url
-[install]
-$pip_trusted_host
-use_wheel = $pip_use_wheel
-EOF
-
-chown "$(stat -c '%U' $HDIR)" -R $HDIR/.pydistutils.cfg $HDIR/.pip
-
-done
+## # patch pip/easy_install config
+##
+## pip_index_url="index_url = http://${mirror}/root/pypi/+simple"
+## pip_trusted_host="trusted-host = ${mirror}"
+## pip_use_wheel="True"
+##
+## for HDIR in /root $(find /home -maxdepth 1 -type d|grep -v '/home$'); do
+##     [[ -d $HDIR ]] || continue
+##
+## cat > $HDIR/.pydistutils.cfg <<EOF
+## [easy_install]
+## $pip_index_url
+## EOF
+##
+## mkdir -p $HDIR/.pip;
+## cat > $HDIR/.pip/pip.conf <<EOF
+## [global]
+## $pip_index_url
+## [install]
+## $pip_trusted_host
+## use_wheel = $pip_use_wheel
+## EOF
+##
+## chown "$(stat -c '%U' $HDIR)" -R $HDIR/.pydistutils.cfg $HDIR/.pip
+##
+## done
