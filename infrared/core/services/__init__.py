253835c34fd22f3ffeb38d92544a8e3f9fcd1ac3
Yair Fried <yfried@redhat.com>
2016-12-08 14:21:29 +0200

    Port profile manager
    Adds only profile essential code.
    Also setup and selinux fix
    
    Change-Id: I2595f3aab90014831c182ff7f05ea8673f0a3ca3

---
+++ infrared/core/services/__init__.py
@@ -0,0 +1,59 @@
+"""Service locator for the IR services
+
+Stores and resolves all the dependencies for the services.
+"""
+import os
+
+from infrared.core.services import profiles
+
+
+class CoreServices(object):
+    """
+    Holds and configures all the required for core services
+    """
+
+    SERVICES = {}
+
+    DEFAULTS = {
+        'profiles_base_folder': '.profiles',
+    }
+
+    @classmethod
+    def from_defaults(cls):
+        """
+        Configures services using the default settings.
+        """
+        cls._configure(
+            cls.DEFAULTS['profiles_base_folder']
+            )
+
+    @classmethod
+    def from_ini_file(cls, config_file, section='core'):
+        """
+        Reads core configuration and configures all the required services.
+        """
+        import ConfigParser
+        config = ConfigParser.ConfigParser()
+        config.read(config_file)
+
+        profile_dir = os.path.abspath(
+            config.get(section, 'profiles_base_folder'))
+
+        cls._configure(profile_dir)
+
+    @classmethod
+    def _configure(cls, profile_dir):
+        cls.SERVICES['profile_manager'] = profiles.ProfileManager(profile_dir)
+
+    @classmethod
+    def _get_service(cls, name):
+        if name not in cls.SERVICES:
+            cls.from_defaults()
+        return cls.SERVICES[name]
+
+    @classmethod
+    def profile_manager(cls):
+        """
+        Gets the profile manager
+        """
+        return cls._get_service('profile_manager')
---
+++ infrared/main.py
@@ -0,0 +1,96 @@
+import sys
+
+from tabulate import tabulate
+
+from infrared import api
+from infrared.core.services import CoreServices
+from infrared.core.utils import logger
+
+PLUGIN_SETTINGS = 'plugins/settings.yml'
+
+LOG = logger.LOG
+
+
+class ProfileManagerSpec(api.SpecObject):
+    """
+    THe profile manager CLI.
+    """
+
+    def __init__(self, name, *args, **kwargs):
+        super(ProfileManagerSpec, self).__init__(name, *args, **kwargs)
+        self.profile_manager = None
+
+    def extend_cli(self, root_subparsers):
+        parser_plugin = root_subparsers.add_parser(self.name, **self.kwargs)
+        plugins_subparsers = parser_plugin.add_subparsers(dest="command0")
+
+        # create
+        create_parser = plugins_subparsers.add_parser(
+            'create', help='Creates a new profile')
+        create_parser.add_argument("name", help="Profie name")
+
+        # activate
+        activate_parser = plugins_subparsers.add_parser(
+            'activate', help='Activates a profile')
+        activate_parser.add_argument("name", help="Profie name")
+
+        # list
+        plugins_subparsers.add_parser(
+            'list', help='Lists all the profiles')
+
+        # delete
+        delete_parser = plugins_subparsers.add_parser(
+            'delete', help='Deletes a profile')
+        delete_parser.add_argument("name", help="Profie name")
+
+        # cleanup
+        cleanup_parser = plugins_subparsers.add_parser(
+            'cleanup', help='Removes all the files from profile')
+        cleanup_parser.add_argument("name", help="Profie name")
+
+    def spec_handler(self, parser, args):
+        """
+        Handles all the plugin manager commands
+        :param parser: the infrared parser object.
+        :param args: the list of arguments received from cli.
+        """
+        command0 = args.get('command0', '')
+
+        profile_manager = CoreServices.profile_manager()
+        if command0 == 'create':
+            profile_manager.create(args.get('name'))
+            print("Profile '{}' added".format(args.get('name')))
+        elif command0 == 'activate':
+            profile_manager.activate(args.get('name'))
+            print("Profile '{}' activated".format(args.get('name')))
+        elif command0 == 'list':
+            profiles = profile_manager.list()
+            print(
+                tabulate([[p.name, profile_manager.is_active(p.name)]
+                         for p in profiles],
+                         headers=("Name", "Is Active"),
+                         tablefmt='orgtbl'))
+        elif command0 == 'delete':
+            profile_manager.delete(args.get('name'))
+            print("Profile '{}' deleted".format(args.get('name')))
+        elif command0 == 'cleanup':
+            profile_manager.cleanup(args.get('name'))
+
+
+def main():
+    # configure core services
+    CoreServices.from_ini_file('infrared.cfg')
+
+    specs_manager = api.SpecManager()
+
+    specs_manager.register_spec(
+        ProfileManagerSpec('profile',
+                           description="Profile manager. "
+                                       "Allows to create and use an isolated "
+                                       "environement  for plugins execution.")
+    )
+
+    specs_manager.run_specs()
+
+if __name__ == '__main__':
+    sys.exit(int(main() or 0))
