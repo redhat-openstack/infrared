a384157e1ea457a3b9c8f9d7e046fbfec1945062
Ariel Opincaru <aopincar@redhat.com>
2017-01-02 11:45:50 +0200

    Removes use in Singleton with InfraRedPlugin class
    Currently not needed and causes some unittests to be failed.
    
    Change-Id: I31ddc87c48938f6c3b258a9ea301f39691f4f033

--- infrared/core/services/plugins.py
+++ infrared/core/services/plugins.py
@@ -20,18 +20,7 @@ MAIN_PLAYBOOK = "main.yml"
 LOG = logger.LOG
 
 
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = \
-                super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
 class InfraRedPluginManager(object):
-    __metaclass__ = Singleton
 
     PLUGINS_DICT = OrderedDict()
     SUPPORTED_TYPES_SECTION = 'supported_types'
