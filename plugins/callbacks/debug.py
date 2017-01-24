7b69dc475c01872b022b69146d5d0dd5d4b8e890
Sorin Sbarnea <ssbarnea@redhat.com>
2017-01-24 13:00:01 +0000

    Backported  debug(human_log) callback from ansible 2.2
    human_log became part of ansible 2.2 with its name changed to "debug".
    Any ansible 2.2 user can enable it by adding stdout_callback = debug
    inside ansible.cfg. This change enables the same behaviour on older versions
    of ansible. Once we bump to ansible 2.2 we will able ale to remove the
    debug.py from our codebase.
    
    Change-Id: I5b120fc372cd9f29576f012c2707b8fc997d9fe5

---
+++ plugins/callbacks/debug.py
@@ -0,0 +1,56 @@
+# Copyright 2016 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+""" debug module is the new name of the human_readably module """
+
+from __future__ import (absolute_import, division, print_function)
+from ansible.plugins.callback.default \
+    import CallbackModule as CallbackModule_default
+
+__metaclass__ = type
+
+
+class CallbackModule(CallbackModule_default):  # pylint: \
+    # disable=too-few-public-methods,no-init
+    '''
+    Override for the default callback module.
+
+    Render std err/out outside of the rest of the result which it prints with
+    indentation.
+    '''
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'stdout'
+    CALLBACK_NAME = 'debug'
+
+    def _dump_results(self, result):
+        '''Return the text to output for a result.'''
+
+        # Enable JSON identation
+        result['_ansible_verbose_always'] = True
+
+        save = {}
+        for key in ['stdout', 'stdout_lines', 'stderr', 'stderr_lines', 'msg']:
+            if key in result:
+                save[key] = result.pop(key)
+
+        output = CallbackModule_default._dump_results(self, result)
+
+        for key in ['stdout', 'stderr', 'msg']:
+            if key in save and save[key]:
+                output += '\n\n%s:\n\n%s\n' % (key.upper(), save[key])
+
+        for key, value in save.items():
+            result[key] = value
+
+        return output
