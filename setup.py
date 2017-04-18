0550b26d4de58f7cd3042e6a171fcf5a22d2cd8e
Luigi Toscano <ltoscano@redhat.com>
2017-04-18 19:33:23 +0200

    Switch to pbr/setup.cfg
    pbr is already a dependency, so use it and switch to the
    declarative setup.cfg.
    
    Change-Id: I50ca8baa303ec5b8eddea272ec0096fcc3d012a6

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
-from setuptools import setup, find_packages
 import platform
-from pip import req
+import setuptools
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = req.parse_requirements('requirements.txt', session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
-
-setup(
-    name='infrared',
-    version='2.0.0',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            "infrared = infrared.main:main",
-            'ir = infrared.main:main'
-        ]
-    },
-    install_requires=reqs,
-    author='',
-    author_email=''
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
 
 if all(platform.linux_distribution(supported_dists="redhat")):
     print("Fixing selinux for redhat systems...")
