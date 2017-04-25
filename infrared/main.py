f59fe975e59c62982b0a5744c1b60292d48b9321
Oleh Anufriiev <oanufrii@redhat.com>
2017-04-25 03:58:57 +0300

    'workspace export' proper handling of external keys
    RHOSINFRA-966
    
    Change-Id: I6fc2842e6d38504345b0ad0971c12c15f9ec31c8

--- infrared/main.py
+++ infrared/main.py
@@ -77,6 +77,10 @@ class WorkspaceManagerSpec(api.SpecObject):
         exporter_parser.add_argument("-f", "--filename", dest="filename",
                                      help="Archive file name.")
 
+        exporter_parser.add_argument("-K", "--copy-keys", dest="copykeys",
+                                     action="store_true",
+                                     help="Silently copy ssh keys "
+                                     "to workspace.")
         # node list
         nodelist_parser = workspace_subparsers.add_parser(
             'node-list',
@@ -113,7 +117,7 @@ class WorkspaceManagerSpec(api.SpecObject):
             self.workspace_manager.cleanup(pargs.name)
         elif subcommand == 'export':
             self.workspace_manager.export_workspace(
-                pargs.workspacename, pargs.filename)
+                pargs.workspacename, pargs.filename, pargs.copykeys)
         elif subcommand == 'import':
             self.workspace_manager.import_workspace(
                 pargs.filename, pargs.workspacename)
