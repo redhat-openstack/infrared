159d118a6deeee19387356703afbf0985bc4c35b
Yair Fried <yfried@redhat.com>
2017-07-12 09:26:16 +0300

    BUGFIX No "baremetal list" in Liberty openstack client
    Add tests to make sure from_csv handles empty list.
    
    Change-Id: I3167ade1ab0084bfcb4a2f6eb3193bed33e19576

--- plugins/tripleo-overcloud/filter_plugins/from_csv.py
+++ plugins/tripleo-overcloud/filter_plugins/from_csv.py
@@ -8,7 +8,15 @@ def csv_to_dict(value, delimiter=","):
 '"c11b095b","foo","power on"']))
     [{'State': 'power off', 'UUID': 'af6a3419', 'Name': ''}, \
 {'State': 'power on', 'UUID': 'c11b095b', 'Name': 'foo'}]
+    >>> csv_to_dict('"UUID","Name","State"\\n')
+    []
+    >>> csv_to_dict('')
+    []
+    >>> csv_to_dict('"UUID","Name","State"')
+    []
     """
+    if not value:
+        return []
     fieldnames = [i.strip('"') for i in value.splitlines()[0].split(delimiter)]
     return [i for i in csv.DictReader(value.splitlines()[1:],
                                       fieldnames=fieldnames)]
