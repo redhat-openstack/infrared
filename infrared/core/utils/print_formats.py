764fe58166634f5fa3aa6fed6608a41d9819d3c2
Sorin Sbarnea <ssbarnea@redhat.com>
2017-09-04 18:47:33 +0100

    use ASCII table headers
    That's safe as it renders ok on any platform.
    
    Change-Id: I912f30bf0e233b767f33df1ff8c9f20a6f084f02

--- infrared/core/utils/print_formats.py
+++ infrared/core/utils/print_formats.py
@@ -1,7 +1,11 @@
-from terminaltables import SingleTable
+import sys
+
+# Jenkins console works with coloring but not with Unicode based tables
+# Non ASCII will still fail on some cases as it depends on too many factors:
+# slave, master and browser, fonts and encodings.
+from terminaltables import AsciiTable
 
 
-# TODO(aopincar): Highlight table header
 def fancy_table(table_headers, *table_rows):
     """Creates a fancy table string from the given data
 
@@ -9,10 +13,12 @@ def fancy_table(table_headers, *table_rows):
     :param table_rows: Iterable elements contain the table rows (body)
     :return: Formatted string represents the newly created table
     """
-    table_data = [table_headers]
+    # coloring class degrates gracefully if output is redirected so we will
+    # not endup with ANSI escapes if we redirect console.
+    table_data = [[c for c in table_headers]]
     table_data.extend(table_rows)
 
-    table = SingleTable(table_data=table_data)
+    table = AsciiTable(table_data=table_data)
     table.inner_row_border = True
 
     return table.table
