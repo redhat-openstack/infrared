c4174414f2a6b6be47cf95a8c782adbc6957492b
Sorin Sbarnea <ssbarnea@redhat.com>
2017-09-04 11:51:03 +0100

    tox-gate.sh: fail if checkout is not clean
    - fail gate if repository is dirty after execution
    - display dirty reason on STDERR
    - silent if everything is ok
    - use ANSI RED to improve visibility
    
    Change-Id: I12b3e325eca547945fe776cbc6548fc4e06132e7

--- tox-gate.sh
+++ tox-gate.sh
@@ -7,3 +7,11 @@ export TOXENV="pep8,py27,cli,ansible-lint,any-errors-fatal,conflicts,plugin-regi
 # python -m detox
 
 python -m tox
+
+if [[ ! -z $(git status -s) ]]; then
+    ANSI_RED="\033[01;31m"
+    ANSI_NORMAL="\033[0m"
+    >&2 git status
+    >&2 echo -e "${ANSI_RED}====== ^^ Failed: Modified files on disk, avoid them or update .gitignore file. ^^ ======${ANSI_NORMAL}"
+    exit 1
+fi
