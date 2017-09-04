420b0fe04750a4aefb45c6cf253c616dda86c3e4
Sorin Sbarnea <ssbarnea@redhat.com>
2017-09-04 11:51:03 +0100

    tox-gate.sh: fail if checkout is not clean
    Change-Id: I12b3e325eca547945fe776cbc6548fc4e06132e7

--- tox-gate.sh
+++ tox-gate.sh
@@ -7,3 +7,9 @@ export TOXENV="pep8,py27,cli,ansible-lint,any-errors-fatal,conflicts,plugin-regi
 # python -m detox
 
 python -m tox
+
+if [[ ! -z $(git status -s) ]]; then
+    git status
+    echo -e "\033[01;31m====== ^^ Failed: Modified files on disk, avoid them or update .gitignore file. ^^ ======\033[0m"
+    exit 1
+fi
