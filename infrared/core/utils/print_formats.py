d1a1974c51a1d0dcb7c4ceb0d18f843b4784690c
Sorin Sbarnea <ssbarnea@redhat.com>
2017-09-04 18:47:33 +0100

    tables: ASCII with colored header
    - fixed invalid table rendering under Jenkins
    - enabled coloring of table headers
    
    Change-Id: I912f30bf0e233b767f33df1ff8c9f20a6f084f02

--- infrared/core/utils/print_formats.py
+++ infrared/core/utils/print_formats.py
@@ -1,7 +1,8 @@
-from terminaltables import SingleTable
+from colorclass import Color
+from terminaltables import AsciiTable
+# do not use SingleTable as it breaks under Jenkins console
 
 
-# TODO(aopincar): Highlight table header
 def fancy_table(table_headers, *table_rows):
     """Creates a fancy table string from the given data
 
@@ -9,10 +10,11 @@ def fancy_table(table_headers, *table_rows):
     :param table_rows: Iterable elements contain the table rows (body)
     :return: Formatted string represents the newly created table
     """
-    table_data = [table_headers]
+    table_data = [[Color('{autoblue}%s{/autoblue}' % c)
+                   for c in table_headers]]
     table_data.extend(table_rows)
 
-    table = SingleTable(table_data=table_data)
+    table = AsciiTable(table_data=table_data)
     table.inner_row_border = True
 
     return table.table
