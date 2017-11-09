1b956474bce14b1595e45441a3ac6c68c3b478c9
obaranov <obaranov@redhat.com>
2017-11-09 18:42:18 +0200

    Infrared custom home directory
    Allows to specify custom home directory where
    infrared will store all the confiurations and
    workspaces.
    
    So now infrared can be run with:
    INFRARED_HOME=~/.infrared ir [....]
    
    Major changes:
    1. Removed ansible.cfg file and reworked configuration.
    Removed static variables to simplify testing and usage.
    2. All the SpecObject's  are now moved to the api.py.
    Main.py now has only SpecManager and initilization.
    3. Added unit test for CoreServices config.
    4. Replaced config setter with _configure method
    to allow passing extra variables to plugin config.
    This ie need to skip plugins installation for
    unit testing.
    
    For now do not touch plugins. Plugins will be moved
    in the separate patch.
    
    Change-Id: Ic666cc62a118eb4e11f27d923ecaac299fdfe3d3

--- infrared/main.py
+++ infrared/main.py
@@ -1,8 +1,9 @@
-import os
-from pbr import version
-import pkg_resources as pkg
 import sys
+
 import argcomplete
+import os
+import pkg_resources as pkg
+from pbr import version
 
 
 def inject_common_paths():
@@ -36,416 +37,16 @@ def inject_common_paths():
 # code (or on demand), then this call can be moved as well in that place.
 inject_common_paths()
 
-
-from infrared import api  # noqa
 from infrared.core.services import CoreServices  # noqa
-from infrared.core.services.plugins import PLUGINS_REGISTRY  # noqa
-from infrared.core.utils import exceptions  # noqa
-from infrared.core.utils import logger  # noqa
-from infrared.core.utils import interactive_ssh  # noqa
-from infrared.core.utils.print_formats import fancy_table  # noqa
-import bash_completers as completers
-
-LOG = logger.LOG
-
-
-class WorkspaceManagerSpec(api.SpecObject):
-    """The workspace manager CLI. """
-
-    def __init__(self, name, *args, **kwargs):
-        super(WorkspaceManagerSpec, self).__init__(name, **kwargs)
-        self.workspace_manager = CoreServices.workspace_manager()
-
-    def extend_cli(self, root_subparsers):
-        workspace_plugin = root_subparsers.add_parser(
-            self.name,
-            help=self.kwargs["description"],
-            **self.kwargs)
-        workspace_subparsers = workspace_plugin.add_subparsers(dest="command0")
-
-        # create
-        create_parser = workspace_subparsers.add_parser(
-            'create', help='Creates a new workspace')
-        create_parser.add_argument("name", help="Workspace name")
-
-        # checkout
-        checkout_parser = workspace_subparsers.add_parser(
-            'checkout',
-            help='Creates a workspace if it is not presents and '
-                 'switches to it')
-        checkout_parser.add_argument(
-            "name",
-            help="Workspace name").completer = completers.workspace_list
-
-        # inventory
-        inventory_parser = workspace_subparsers.add_parser(
-            'inventory',
-            help="prints workspace's inventory file")
-        inventory_parser.add_argument(
-            "name", help="Workspace name",
-            nargs="?").completer = completers.workspace_list
-
-        # list
-        wrkspc_list_parser = workspace_subparsers.add_parser(
-            'list', help='Lists all the workspaces')
-        wrkspc_list_parser.add_argument(
-            "--active", action='store_true', dest='print_active',
-            help="Prints the active workspace only")
-
-        # delete
-        delete_parser = workspace_subparsers.add_parser(
-            'delete', help='Deletes workspaces')
-        delete_parser.add_argument(
-            'name', nargs='+',
-            help="Workspace names").completer = completers.workspace_list
-
-        # cleanup
-        cleanup_parser = workspace_subparsers.add_parser(
-            'cleanup', help='Removes all the files from workspace')
-        cleanup_parser.add_argument(
-            "name",
-            help="Workspace name").completer = completers.workspace_list
-
-        # import settings
-        importer_parser = workspace_subparsers.add_parser(
-            'import', help='Import deployment configs.')
-        importer_parser.add_argument("filename", help="Archive file name.")
-        importer_parser.add_argument(
-            "-n", "--name", dest="workspacename",
-            help="Workspace name to import with. "
-            "If not specified - file name will be used.")
-
-        # exort settings
-        exporter_parser = workspace_subparsers.add_parser(
-            'export', help='Export deployment configurations.')
-        exporter_parser.add_argument(
-            "-n", "--name", dest="workspacename",
-            help="Workspace name. If not sepecified - active "
-            "workspace will be used.").completer = completers.workspace_list
-        exporter_parser.add_argument("-f", "--filename", dest="filename",
-                                     help="Archive file name.")
-
-        exporter_parser.add_argument("-K", "--copy-keys", dest="copykeys",
-                                     action="store_true",
-                                     help="Silently copy ssh keys "
-                                     "to workspace.")
-        # node list
-        nodelist_parser = workspace_subparsers.add_parser(
-            'node-list',
-            help='List nodes, managed by workspace')
-        nodelist_parser.add_argument(
-            "-n", "--name",
-            help="Workspace name").completer = completers.workspace_list
-        nodelist_parser.add_argument(
-            "-g", "--group",
-            help="List nodes in specific group"
-            ).completer = completers.group_list
-
-        # group list
-        grouplist_parser = workspace_subparsers.add_parser(
-            'group-list',
-            help='List groups, managed by workspace')
-        grouplist_parser.add_argument(
-            "-n", "--name",
-            help="Workspace name").completer = completers.workspace_list
-
-    def spec_handler(self, parser, args):
-        """
-        Handles all the plugin manager commands
-        :param parser: the infrared parser object.
-        :param args: the list of arguments received from cli.
-        """
-        pargs = parser.parse_args(args)
-        subcommand = pargs.command0
-
-        if subcommand == 'create':
-            self._create_workspace(pargs.name)
-        elif subcommand == 'checkout':
-            self._checkout_workspace(pargs.name)
-        elif subcommand == 'inventory':
-            self._fetch_inventory(pargs.name)
-        elif subcommand == 'list':
-            if pargs.print_active:
-                print self.workspace_manager.get_active_workspace().name
-            else:
-                workspaces = self.workspace_manager.list()
-                headers = ("Name", "Active")
-                workspaces = sorted([workspace.name for workspace in
-                                     self.workspace_manager.list()])
-                print fancy_table(
-                    headers,
-                    *[(workspace, ' ' * (len(headers[-1]) / 2) + "*" if
-                        self.workspace_manager.is_active(workspace) else "")
-                      for workspace in workspaces])
-        elif subcommand == 'delete':
-            for workspace_name in pargs.name:
-                self.workspace_manager.delete(workspace_name)
-                print("Workspace '{}' deleted".format(workspace_name))
-        elif subcommand == 'cleanup':
-            self.workspace_manager.cleanup(pargs.name)
-        elif subcommand == 'export':
-            self.workspace_manager.export_workspace(
-                pargs.workspacename, pargs.filename, pargs.copykeys)
-        elif subcommand == 'import':
-            self.workspace_manager.import_workspace(
-                pargs.filename, pargs.workspacename)
-        elif subcommand == 'node-list':
-            nodes = self.workspace_manager.node_list(pargs.name, pargs.group)
-            print fancy_table(
-                ("Name", "Address", "Groups"),
-                *[node_name for node_name in nodes])
-        elif subcommand == "group-list":
-            groups = self.workspace_manager.group_list(pargs.name)
-            print fancy_table(
-                ("Name", "Nodes"), *[group_name for group_name in groups])
-
-    # deprecated method
-    def _create_workspace(self, name):
-        """Creates a workspace """
-
-        self._checkout_workspace(name)
-
-    def _checkout_workspace(self, name):
-        """Checkouts (create+activate) a workspace """
-
-        if not self.workspace_manager.has_workspace(name):
-            self.workspace_manager.create(name)
-            print("Workspace '{}' added".format(name))
-        self.workspace_manager.activate(name)
-        print("Now using workspace: '{}'".format(name))
-
-    def _fetch_inventory(self, name):
-        """fetch inventory file for workspace.
-
-        if no active workspace found - create a new workspace
-        """
-        if name:
-            wkspc = self.workspace_manager.get(name)
-        else:
-            wkspc = self.workspace_manager.get_active_workspace()
-        if not wkspc:
-            raise exceptions.IRNoActiveWorkspaceFound()
-        print wkspc.inventory
-
-
-class PluginManagerSpec(api.SpecObject):
-
-    def __init__(self, name, plugin_manager, *args, **kwargs):
-        self.plugin_manager = plugin_manager
-        super(PluginManagerSpec, self).__init__(name, *args, **kwargs)
-
-    def extend_cli(self, root_subparsers):
-        plugin_parser = root_subparsers.add_parser(
-            self.name,
-            help=self.kwargs["description"],
-            **self.kwargs)
-        plugin_subparsers = plugin_parser.add_subparsers(dest="command0")
-
-        # Add plugin
-        add_parser = plugin_subparsers.add_parser(
-            'add', help='Add a plugin')
-        add_parser.add_argument("src",
-                                help="Plugin Source (name/path/git URL)\n'all'"
-                                     " will install all available plugins")
-        add_parser.add_argument("--revision", help="git branch/tag/revision"
-                                " sourced plugins. Ingnored for"
-                                "'plugin add all' command.")
-        add_parser.add_argument("--dest", help="Destination directory to "
-                                "clone plugin under, in case of Git URL is "
-                                "provided as path")
-
-        # Remove plugin
-        remove_parser = plugin_subparsers.add_parser(
-            "remove",
-            help="Remove a plugin, 'all' will remove all installed plugins")
-        remove_parser.add_argument(
-            "name",
-            help="Plugin name").completer = completers.plugin_list
-
-        # List command
-        list_parser = plugin_subparsers.add_parser(
-            'list', help='List all the available plugins')
-        list_parser.add_argument(
-            "--available", action='store_true',
-            help="Prints all available plugins in addition "
-                 "to installed plugins")
-
-        # Update plugin
-        update_parser = plugin_subparsers.add_parser(
-            "update",
-            help="Update a Git-based plugin")
-        update_parser.add_argument(
-            "name",
-            help="Name of the plugin to update")
-        update_parser.add_argument(
-            "revision", nargs='?', default='latest',
-            help="Revision number to checkout (if not given, will only pull "
-                 "changes from the remote)")
-        update_parser.add_argument(
-            '--skip_reqs', '-s', action='store_true',
-            help="Skips plugin's requirements installation")
-        update_parser.add_argument(
-            '--hard-reset', action='store_true',
-            help="Drop all local changes using hard "
-                 "reset (changes will be stashed")
-
-        plugin_subparsers.add_parser(
-            "freeze", help="Run through installed plugins. For git sourced "
-            "one writes its current revision to plugins registry.")
-
-        # search all plugins from github organization
-        plugin_subparsers.add_parser(
-            'search', help='Search and list all the available plugins from '
-            "rhos-infra organization on GitHub")
-
-    def spec_handler(self, parser, args):
-        """Handles all the plugin manager commands
-
-        :param parser: the infrared parser object.
-        :param args: the list of arguments received from cli.
-        """
-        pargs = parser.parse_args(args)
-        subcommand = pargs.command0
-
-        if subcommand == 'list':
-            self._list_plugins(pargs.available)
-        elif subcommand == 'add':
-            if pargs.src == 'all':
-                self.plugin_manager.add_all_available()
-                self._list_plugins(print_available=False)
-            else:
-                self.plugin_manager.add_plugin(pargs.src, rev=pargs.revision,
-                                               dest=pargs.dest)
-        elif subcommand == 'remove':
-            if pargs.name == 'all':
-                self.plugin_manager.remove_all()
-                self._list_plugins(print_available=False)
-            else:
-                self.plugin_manager.remove_plugin(pargs.name)
-        elif subcommand == 'freeze':
-            self.plugin_manager.freeze()
-        elif subcommand == 'update':
-            self.plugin_manager.update_plugin(
-                pargs.name, pargs.revision, pargs.skip_reqs, pargs.hard_reset)
-        elif subcommand == 'search':
-            self._search_plugins()
-
-    def _list_plugins(self, print_available=False):
-        """Print a list of installed & available plugins"""
-        table_rows = []
-        table_headers = ["Type", "Name"]
-        installed_mark = ' ' * (len('Installed') / 2) + '*'
-
-        plugins_dict = \
-            self.plugin_manager.get_all_plugins() \
-            if print_available \
-            else self.plugin_manager.get_installed_plugins()
-
-        for plugins_type, plugins in plugins_dict.iteritems():
-            installed_plugins_list = \
-                self.plugin_manager.get_installed_plugins(plugins_type).keys()
-            plugins_names = plugins.keys()
-            plugins_names.sort()
-
-            if print_available:
-                all_plugins_list = []
-                for plugin_name in plugins_names:
-                    all_plugins_list.append(plugin_name)
-                installed_plugins_mark_list = \
-                    [installed_mark if plugin_name in installed_plugins_list
-                     else '' for plugin_name in all_plugins_list]
-
-                plugins_descs = \
-                    [PLUGINS_REGISTRY.get(plugin, {}).get('desc', '')
-                     for plugin in all_plugins_list]
-
-                table_rows.append([
-                    plugins_type,
-                    '\n'.join(all_plugins_list),
-                    '\n'.join(installed_plugins_mark_list),
-                    '\n'.join(plugins_descs)])
-            else:
-                table_rows.append([
-                    plugins_type,
-                    '\n'.join(installed_plugins_list)])
-
-        if print_available:
-            table_headers.append("Installed")
-            table_headers.append("Description")
-
-        print fancy_table(table_headers, *table_rows)
-
-    def _search_plugins(self):
-        """
-        Search git organizations and print a list of available plugins
-        """
-        table_rows = []
-        table_headers = ["Type", "Name", "Description", "Source"]
-
-        plugins_dict = \
-            self.plugin_manager.get_all_git_plugins()
-
-        for plugins_type, plugins in plugins_dict.iteritems():
-            # prepare empty lists
-            all_plugins_list = []
-            plugins_descs = []
-            plugins_sources = []
-
-            for plugin_name in sorted(plugins.iterkeys()):
-                # get all plugin names
-                all_plugins_list.append(plugin_name)
-                # get all plugin descriptions
-                plugins_descs.append(plugins[plugin_name]["desc"])
-                # get all plugins sources
-                plugins_sources.append(plugins[plugin_name]["src"])
-
-            table_rows.append([
-                plugins_type,
-                '\n'.join(all_plugins_list),
-                '\n'.join(plugins_descs),
-                '\n'.join(plugins_sources)])
-
-        print(fancy_table(table_headers, *table_rows))
-
-
-class SSHSpec(api.SpecObject):
-
-    def __init__(self, name, *args, **kwargs):
-        super(SSHSpec, self).__init__(name, *args, **kwargs)
-
-    def extend_cli(self, root_subparsers):
-        issh_parser = root_subparsers.add_parser(
-            self.name,
-            help=self.kwargs["description"],
-            **self.kwargs)
-
-        issh_parser.add_argument("node_name", help="Node name. "
-                                 "Ex.: controller-0"
-                                 ).completer = completers.node_list
-        issh_parser.add_argument("remote_command", nargs="?", help="Run "
-                                 "provided command line on remote host and "
-                                 "return its output.")
-
-    def spec_handler(self, parser, args):
-        """Handles the ssh command
-
-        :param parser: the infrared parser object.
-        :param args: the list of arguments received from cli.
-        """
-        pargs = parser.parse_args(args)
-        return interactive_ssh.ssh_to_host(
-            pargs.node_name, remote_command=pargs.remote_command)
+from infrared.api import WorkspaceManagerSpec, PluginManagerSpec, SSHSpec, \
+    InfraredPluginsSpec, SpecManager  # noqa
 
 
 def main(args=None):
-    # configure core services
-    CoreServices.setup('infrared.cfg')
+    CoreServices.setup()
+    specs_manager = SpecManager()
 
     # Init Managers
-    plugin_manager = CoreServices.plugins_manager()
-
-    specs_manager = api.SpecManager()
-
     specs_manager.register_spec(
         WorkspaceManagerSpec('workspace',
                              description="Workspace manager. "
@@ -454,7 +55,6 @@ def main(args=None):
                                          "execution."))
     specs_manager.register_spec(
         PluginManagerSpec('plugin',
-                          plugin_manager=plugin_manager,
                           description="Plugin management"))
 
     specs_manager.register_spec(
@@ -463,12 +63,11 @@ def main(args=None):
             description="Interactive ssh session to node from inventory."))
 
     # register all plugins
-    for plugin in plugin_manager.PLUGINS_DICT.values():
-        specs_manager.register_spec(api.InfraredPluginsSpec(plugin))
+    for plugin in CoreServices.plugins_manager().PLUGINS_DICT.values():
+        specs_manager.register_spec(InfraredPluginsSpec(plugin))
 
     argcomplete.autocomplete(specs_manager.parser)
     return specs_manager.run_specs(args) or 0
-
 
 if __name__ == '__main__':
     sys.exit(int(main() or 0))
