c4ab9effcd5f25e06e8954e1833906300053c230
Vadim Griner <vgriner@redhat.com>
2018-01-01 15:21:10 +0200

    Choose localhost as hypervisor
    Add the ability to run Virsh as localhost when InfraRed
    is installed directly on target.
    
    RHOSINFRA-360
    
    Change-Id: I58c8ce2b3fb42532c3a1ca848defe90893ac6368

--- infrared/core/inspector/inspector.py
+++ infrared/core/inspector/inspector.py
@@ -371,17 +371,31 @@ class SpecParser(object):
 
                 # validate conditions
                 for req_when_arg in req_when_args:
-                    req_arg, req_value = req_when_arg.split('==')
-                    req_value = yaml.load(str(req_value))
-                    actual_value = \
-                        args.get(command_name, {}).get(req_arg.strip(), None)
-                    actual_value = yaml.load(str(actual_value))
-                    if actual_value == req_value \
+                    if "==" in req_when_arg:
+                        req_arg, req_value = req_when_arg.split('==')
+                        req_value = yaml.load(str(req_value))
+                        actual_value = \
+                            args.get(command_name, {}).get(req_arg.strip(), None)
+                        actual_value = yaml.load(str(actual_value))
+                        if actual_value == req_value \
                             and self.spec_helper.get_option_state(
                                 command_name,
                                 option_spec['name'],
-                                args) == helper.OptionState['NOT_SET']:
-                        res.append(option_spec['name'])
+                            args) == helper.OptionState['NOT_SET']:
+                            res.append(option_spec['name'])
+
+                    if "!=" in req_when_arg:
+                        req_arg, req_value = req_when_arg.split('!=')
+                        req_value = yaml.load(str(req_value))
+                        actual_value = \
+                            args.get(command_name, {}).get(req_arg.strip(), None)
+                        actual_value = yaml.load(str(actual_value))
+                        if actual_value != req_value \
+                            and self.spec_helper.get_option_state(
+                                command_name,
+                                option_spec['name'],
+                            args) == helper.OptionState['NOT_SET']:
+                            res.append(option_spec['name'])
         return res
 
     def validate_requires_args(self, args):
