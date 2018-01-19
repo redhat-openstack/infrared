0bfdaa6c41c48f49b5bca6f7bc56ba32ef1fa88a
Ruslan Usichenko <rusichen@redhat.com>
2018-01-19 20:41:25 +0200

    [Rally] Add rpm setup strategy
    Added option '--setup' [git,rpm] with default value
    'git' to not break current rally deployment. As we finish
    with migration/testing jenkins jobs with enabled 'rpm' strategy,
    default value for '--setup' option should be changed on 'rpm' and
    default value for options git-repo,git-revision,git-plugins-revision
    could be deleted.
    Added ansible filter called 'try_default' that allows to ignore 'undefined'
    variables and return default value.
    Added restriction for nova-rally test to allow run it on rally version >= 0.10,
    because it requires appropritate commands available only there.
    
    RHOSINFRA-879
    
    Change-Id: Ib1669ccf3e78626586d81b86b5a10138edcc8d49

---
+++ plugins/rally/filter_plugins/try_default.py
@@ -0,0 +1,25 @@
+from jinja2.runtime import Undefined
+from jinja2.exceptions import UndefinedError
+
+def try_default(value, default, ignore=False):
+    """
+    This filter ignores undefined variables and returns 
+    default value, if default value is undefined and 'ignore'
+    options is enabled then return 'False' otherwise raise
+    exception 'UndefinedError'.
+    """
+
+    if not isinstance(value, Undefined):
+        return value
+    elif not isinstance(default, Undefined):
+        return default
+    elif isinstance(default, Undefined) and ignore:
+        return False
+    else:
+        raise UndefinedError("One of the nested variables are undefined")
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'try_default': try_default,
+        }
