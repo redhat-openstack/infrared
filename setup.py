a5adb8aacee36e1833619135e8a09f7fceb107b3
Arie Bregman <abregman@redhat.com>
2018-04-16 00:13:42 +0300

    Adjust setup.py to new pip release
    pip.req is pip._internal.req in pip >= 10.
    
    Change-Id: Idbf17d04e47901deb79a9d114178067139830ff9

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import os
 from os.path import join, dirname, abspath
-from pip import req
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 import platform
 from setuptools import setup, find_packages
 
 import cli
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = req.parse_requirements('requirements.txt', session=False)
+install_reqs = parse_requirements('requirements.txt', session=False)
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
