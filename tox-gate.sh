d8435d7e57d3d2e42a3b66e6b0a6269f3bf59523
Sorin Sbarnea <ssbarnea@redhat.com>
2018-05-08 20:19:58 +0100

    disable parallel tox to avoid flaky failures
    Disables parallel tox execution with
    standard sequential execution.
    
    Keeps comment pointing to pip bug that
    made us disable parallel execution.
    
    Removes bash code that was duplicating
    functionality parallel execution support,
    a feature which is part of tox, module named detox (shares same issue tracker as tox).
    
    Change-Id: I835d0b87fa74bfe0a00a6ee0deb985912129e029
    See: https://github.com/pypa/pip/issues/5345
    (cherry picked from commit 94ed0379cf543949b89d5e9c0adaf24b07563462)

--- tox-gate.sh
+++ tox-gate.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 set -x
-RUN_ENVS="pep8 py27 cli ansible-lint any-errors-fatal conflicts plugin-registry"
+export TOXENV="pep8,py27,cli,ansible-lint,any-errors-fatal,conflicts,plugin-registry"
 
-FAILED=""
-run_tox() {
-    tox -e$1 &> .tox_$1_out &
-}
+# disabled parallel execution due to https://github.com/pypa/pip/issues/5345
+# which detox >/dev/null || pip install --user detox
+# python -m detox
 
-verify_tox() {
-    if ! tail -n2 .tox_$1_out | grep -q "$1: commands succeeded"; then
-        FAILED="${FAILED} $1"
-    else
-        echo -e "\033[01;33m_________ output of $1: _________\033[0m"
-        # failed outputs printed at the end
-        cat .tox_$1_out
-        echo -e "\033[01;33m====== ^^ end of $1 ^^ ======\033[0m"
-    fi
-}
-print_failed() {
-    for tEnv in ${FAILED}; do
-        echo -e "\033[01;33m_________ output of ${tEnv}: _________\033[0m"
-        cat .tox_${tEnv}_out;
-        echo -e "\033[01;31m====== ^^ end of failed ${tEnv} ^^ ======\033[0m"
-    done
-}
-
-for tEnv in ${RUN_ENVS}; do run_tox ${tEnv}; done
-wait
-set +x
-for tEnv in ${RUN_ENVS}; do verify_tox ${tEnv} || exit 1; done
-if [[ ! -z "$FAILED" ]]; then
-    print_failed
-    echo -e "\033[01;31m==== Failed: $FAILED ====\033[0m" >&2
-    exit 1
-fi
+python -m tox
