5ff8c6f42bcbf0ab57172b3135110fa93fadbdaa
Sorin Sbarnea <ssbarnea@redhat.com>
2018-05-08 20:35:42 +0100

    upgraded hacking version
    Switched to current hacking and sorted
    newly detected errors.
    
    Removed pep8 requirement because
    hacking does install specific version.
    
    Change-Id: I1ae07ef0470363708ce04e2edbf84cd34398cc08

--- infrared/common/library/rhos_release.py
+++ infrared/common/library/rhos_release.py
@@ -15,6 +15,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
+import os
+import re
+from ansible.module_utils.basic import AnsibleModule
 
 
 DOCUMENTATION = '''
