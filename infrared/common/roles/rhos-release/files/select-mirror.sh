a0ef71e683334f3e8423d3b4d46bbb660b017f4b
Pavel Sedlák <psedlak@redhat.com>
2018-06-08 15:20:27 +0200

    select-mirror: retry mirror status 3 times
    In case of overloaded network (in locations with bad connectivity),
    it's seems that even access to mirrors can get affected,
    so better to retry obtaining mirror status than quickly skipping it
    
    Change-Id: I2f6ab77ed03aeceafaa34747e70e4b0f48b28e5b

--- infrared/common/roles/rhos-release/files/select-mirror.sh
+++ infrared/common/roles/rhos-release/files/select-mirror.sh
@@ -8,8 +8,14 @@ fi
 
 
 # check that mirror is ready and responds in time
+mirror_status=unreachable
+retries=3
+while [[ "$mirror_status" = "unreachable" && $retries -gt 0 ]]; do
+    retries=$(( $retries - 1 ))
+    mirror_status="$(curl --max-time 3 "http://${mirror}/status" || echo "unreachable")"
+    [[ "$mirror_status" = "unreachable" ]] && echo "WARNING: mirror not reached, remaining $retries attempts ... "
+done
 
-mirror_status="$(curl --max-time 3 "http://${mirror}/status" || echo "unreachable")"
 echo "Mirror ${mirror} status: '$mirror_status'" >&2
 if [[ "$mirror_status" != "ready" ]]; then
     echo "Skipping mirror usage as it is not ready." >&2
