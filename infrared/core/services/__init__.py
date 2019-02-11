321ee7d780ff1ae709b1ad41000e98405e4d4412
amachluf <amachluf@redhat.com>
2019-02-11 13:28:14 +0200

    Enable Infrared to be executed from anywhere
    Make sure all resources Infrared creates are being created under the
    IR_HOME directory, this allows Infrared to be executed from anywhere.
    
    RHOSINFRA-2348
    
    Change-Id: I5631d676844433bd4bc14fb6a0fbcdd87775f7ca

--- infrared/core/services/__init__.py
+++ infrared/core/services/__init__.py
@@ -8,6 +8,7 @@ import sys
 from infrared.core.services import workspaces
 from infrared.core.services import plugins
 from infrared.core.services import ansible_config
+from infrared.core.services import execution_logger
 from infrared.core.utils import logger
 
 LOG = logger.LOG
@@ -18,6 +19,7 @@ class ServiceName(object):
     WORKSPACE_MANAGER = "workspace_manager"
     PLUGINS_MANAGER = "plugins_manager"
     ANSIBLE_CONFIG_MANAGER = "ansible_config_manager"
+    EXECUTION_LOGGER_MANAGER = "execution_logger_manager"
 
 
 class CoreSettings(object):
@@ -100,6 +102,18 @@ class CoreServices(object):
                                  ansible_config.AnsibleConfigManager(
                                      core_settings.infrared_home))
 
+        # create execution logger manager
+        if ServiceName.EXECUTION_LOGGER_MANAGER not in cls._SERVICES:
+            # get ansible manager
+            ansible_manager = CoreServices.ansible_config_manager()
+            # build log file path
+            log_file = \
+                os.path.join(core_settings.infrared_home, 'ir-commands.log')
+            cls.register_service(ServiceName.EXECUTION_LOGGER_MANAGER,
+                                 execution_logger.ExecutionLoggerManager(
+                                     ansible_manager.ansible_config_path,
+                                     log_file=log_file))
+
     @classmethod
     def register_service(cls, service_name, service):
         """Protect the _SERVICES dict"""
@@ -125,3 +139,8 @@ class CoreServices(object):
     def ansible_config_manager(cls):
         """Gets the ansible config manager. """
         return cls._get_service(ServiceName.ANSIBLE_CONFIG_MANAGER)
+
+    @classmethod
+    def execution_logger_manager(cls):
+        """Gets the execution logger manager. """
+        return cls._get_service(ServiceName.EXECUTION_LOGGER_MANAGER)
