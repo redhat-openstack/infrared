99067ff507c0c62f0826bdad105b6bb04a9d551d
Yurii Prokulevych <yprokule@redhat.com>
2019-04-23 06:51:17 +0200

    Update URL for tripleo-upgrade plugin.
    Upstream repos were moved to opendev.org domain and this has to
    be reflacted for tripleo-upgrade plugin, used downstream.
    
    http://lists.openstack.org/pipermail/openstack-discuss/2019-April/005011.html
    
    Change-Id: I71423eac892a9bc8becb89f3f964b5dfe58d6d0b

--- infrared/__init__.py
+++ infrared/__init__.py
@@ -229,7 +229,7 @@ PLUGINS_REGISTRY = {
         'type': 'install'
     },
     'tripleo-upgrade': {
-        'src': 'https://git.openstack.org/openstack/tripleo-upgrade.git',
+        'src': 'https://opendev.org/openstack/tripleo-upgrade.git',
         'src_path': 'infrared_plugin',
         'desc': 'Upgrade or update TripleO deployment',
         'type': 'install'
