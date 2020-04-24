d015afb031b32baecfc1360591345473f56198fc
afazekas <afazekas@redhat.com>
2020-04-24 19:13:29 +0200

    Use base_prefix instead of real_prefix with selinux stuff
    According to the virtualenv issue the real_preafix is not expected in the newer
    style virtual environtment, so let's try base_prefix when selinux
    import fails normally and real_prefix also missing.
    
    [1] https://github.com/pypa/virtualenv/issues/1622
    
    Change-Id: I9b3e8f2d675ecee411621cc8089f198056902e91

--- infrared/core/utils/selinux_fix.py
+++ infrared/core/utils/selinux_fix.py
@@ -13,41 +13,45 @@ def copy_system_selinux(force=True):
         import shutil
 
         if hasattr(sys, 'real_prefix'):
-            # check for venv
-            VENV_SITE = sysconfig.get_python_lib()
-            SELINUX_PATH = os.path.join(
-                sysconfig.get_python_lib(plat_specific=True,
-                                         prefix=sys.real_prefix),
-                "selinux")
-            dest = os.path.join(VENV_SITE, "selinux")
-            if force:
-                shutil.rmtree(dest, ignore_errors=True)
-            elif os.path.exists(dest):
-                raise new_error
-
-            # filter precompiled files including subdirs
-            files = []
-            for path, _, fnames in os.walk(SELINUX_PATH):
-                for name in fnames:
-                    if not os.path.splitext(name)[1] in (".pyc", ".pyo"):
-                        files.append(os.path.join(path, name))
-
-            # add extra file for (libselinux-python-2.5-9.fc24.x86_64)
-            # for python3 we can have extra lines after _selinux names.
-            # so need to use glob expression
-            root_dest = sysconfig.get_python_lib(
-                plat_specific=True, prefix=sys.real_prefix)
-            _selinux_files = glob.glob(os.path.join(root_dest, "_selinux*.so"))
-
-            os.makedirs(dest)
-            for f in files:
-                shutil.copy(f, dest)
-
-            for _selinux_file in _selinux_files:
-                if os.path.exists(_selinux_file):
-                    shutil.copy(_selinux_file, os.path.dirname(dest))
+            fallback_prefix = sys.real_prefix
+        elif hasattr(sys, 'base_prefix'):
+            fallback_prefix = sys.base_prefix
         else:
             raise new_error
+
+        # check for venv
+        VENV_SITE = sysconfig.get_python_lib()
+        SELINUX_PATH = os.path.join(
+            sysconfig.get_python_lib(plat_specific=True,
+                                     prefix=fallback_prefix),
+            "selinux")
+        dest = os.path.join(VENV_SITE, "selinux")
+        if force:
+            shutil.rmtree(dest, ignore_errors=True)
+        elif os.path.exists(dest):
+            raise new_error
+
+        # filter precompiled files including subdirs
+        files = []
+        for path, _, fnames in os.walk(SELINUX_PATH):
+            for name in fnames:
+                if not os.path.splitext(name)[1] in (".pyc", ".pyo"):
+                    files.append(os.path.join(path, name))
+
+        # add extra file for (libselinux-python-2.5-9.fc24.x86_64)
+        # for python3 we can have extra lines after _selinux names.
+        # so need to use glob expression
+        root_dest = sysconfig.get_python_lib(
+            plat_specific=True, prefix=sys.fallback_prefix)
+        _selinux_files = glob.glob(os.path.join(root_dest, "_selinux*.so"))
+
+        os.makedirs(dest)
+        for f in files:
+            shutil.copy(f, dest)
+
+        for _selinux_file in _selinux_files:
+            if os.path.exists(_selinux_file):
+                shutil.copy(_selinux_file, os.path.dirname(dest))
         import selinux  # noqa
 
 
