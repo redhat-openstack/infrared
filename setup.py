bb9e5a05d2cb6888060599f8ee922e0f1e32ac23
Federico Ressi <fressi@redhat.com>
2020-05-07 18:17:50 +0200

    [WIP] Fix installation on Python 3.8
    Change-Id: I5989e9c2c35240b6915b3a4c98dfc7d2e78f56aa

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-import platform
 import setuptools
+import warnings
+
+
+def is_redhat():
+    try:
+        from platform import linux_distribution
+    except ImportError as ex:
+        # Since Python 3.8 linux_distribution has been removed
+        try:
+            import distro
+        except ImportError as ex:
+            warnings.warn(str(ex))
+            return False  # this is fine in most of the cases
+        else:
+            return 'rhel' in distro.like().split()
+    else:
+        return 'redhat' in linux_distribution(supported_dists='redhat',
+                                              full_distribution_name=False)
+
 
 setuptools.setup(
-    setup_requires=['pbr'],
+    setup_requires=['pbr', 'distro'],
     pbr=True)
 
-if 'redhat' in platform.linux_distribution(
-        supported_dists='redhat',
-        full_distribution_name=False):
+
+if is_redhat():
     from infrared.core.utils.selinux_fix import copy_system_selinux
     copy_system_selinux()
