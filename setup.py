e7904209a11e1525edcbce4552d9182dfac875e8
Federico Ressi <fressi@redhat.com>
2020-05-07 18:17:50 +0200

    Fix installation on Python 3.8
    Change-Id: I5989e9c2c35240b6915b3a4c98dfc7d2e78f56aa

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-import platform
 import setuptools
+import importlib
 
 setuptools.setup(
     setup_requires=['pbr'],
     pbr=True)
 
-if 'redhat' in platform.linux_distribution(
-        supported_dists='redhat',
-        full_distribution_name=False):
+
+for module_name in ['platform', 'distro']:
+    module = importlib.import_module(module_name)
+    linux_distribution = getattr(module, 'linux_distribution', None)
+    if linux_distribution:
+        break
+else:
+    def linux_distribution(*_args, **_kwargs):
+        return []
+
+
+if 'redhat' in linux_distribution(supported_dists='redhat',
+                                  full_distribution_name=False):
     from infrared.core.utils.selinux_fix import copy_system_selinux
     copy_system_selinux()
