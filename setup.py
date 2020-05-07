94f02818c9e6fa55323264e149adf49dace13def
Federico Ressi <fressi@redhat.com>
2020-05-07 18:17:50 +0200

    [WIP] Fix installation on Python 3.8
    Change-Id: I5989e9c2c35240b6915b3a4c98dfc7d2e78f56aa

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-import platform
 import setuptools
 
-setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True)
 
-if 'redhat' in platform.linux_distribution(
-        supported_dists='redhat',
-        full_distribution_name=False):
+setuptools.setup(setup_requires=['pbr'], pbr=True)
+
+
+def is_redhat():
+    try:
+        from platform import linux_distribution
+    except ImportError:
+        # Since Python 3.5 linux_distribution has been deprecated and
+        # since Python 3.8 linux_distribution has been removed
+        import distro
+        return 'rhel' in distro.like().split()
+
+    return 'redhat' in linux_distribution(supported_dists='redhat',
+                                          full_distribution_name=False)
+
+
+if is_redhat():
     from infrared.core.utils.selinux_fix import copy_system_selinux
     copy_system_selinux()
