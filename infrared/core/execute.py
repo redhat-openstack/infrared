6f67a1c929c7a58ef3c0d17522d53740a3ebb5db
Ariel Opincaru <aopincar@redhat.com>
2020-07-22 13:24:52 -0400

    Add the ability to manipulate Ansible's output
    The change added the ability to control the output that comes from
    Ansible and to create logs files, with or without ANSI colors, from this
    output using different environment variables.
    
    Log files will be available in the 'ansible_outputs' directory inside
    the active working directory with the following name convention:
    ir_<HR_timestamp>_<plugin_name>.log
    ir_<HR_timestamp>_<plugin_name>_no_ansi.log
    
    The environment variables are:
    * IR_ANSIBLE_NO_STDOUT
    * IR_ANSIBLE_STDERR_TO_STDOUT
    * IR_ANSIBLE_LOG_OUTPUT
    * IR_ANSIBLE_LOG_OUTPUT_NO_ANSI
    
    RHOSINFRA-3242
    
    Change-Id: I619a2d8b2a297900847d14ec12ae4c17fa3310ae

--- infrared/core/execute.py
+++ infrared/core/execute.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from distutils.util import strtobool
+import errno
+import os
+import sys
+import re
 import tempfile
 
 from infrared.core.utils import logger
@@ -6,7 +12,25 @@ import yaml
 LOG = logger.LOG
 
 
-def ansible_playbook(inventory, playbook_path, verbose=None,
+class NoAnsiFile(object):
+
+    re_ansi = re.compile(r'\x1b[^m]*m')
+
+    def __init__(self, fd):
+        self.fd = fd
+
+    def write(self, data):
+        no_ansi_data = self.re_ansi.sub('', data)
+        self.fd.write(no_ansi_data)
+
+    def close(self):
+        self.fd.close()
+
+    def flush(self):
+        self.fd.flush()
+
+
+def ansible_playbook(ir_workspace, ir_plugin, playbook_path, verbose=None,
                      extra_vars=None, ansible_args=None):
     """Wraps the 'ansible-playbook' CLI.
 
@@ -29,7 +53,7 @@ def ansible_playbook(inventory, playbook_path, verbose=None,
     # TODO(yfried): Use proper ansible API instead of emulating CLI
     cli_args = ['execute',
                 playbook_path,
-                '--inventory', inventory]
+                '--inventory', ir_workspace.inventory]
 
     # infrared should not change ansible verbosity unless user specifies that
     if verbose:
@@ -37,15 +61,15 @@ def ansible_playbook(inventory, playbook_path, verbose=None,
 
     cli_args.extend(ansible_args)
 
-    results = _run_playbook(cli_args,
-                            vars_dict=extra_vars or {})
+    results = _run_playbook(cli_args, vars_dict=extra_vars or {},
+                            ir_workspace=ir_workspace, ir_plugin=ir_plugin)
 
     if results:
         LOG.error('Playbook "%s" failed!' % playbook_path)
     return results
 
 
-def _run_playbook(cli_args, vars_dict):
+def _run_playbook(cli_args, vars_dict, ir_workspace, ir_plugin):
     """Runs ansible cli with vars dict
 
     :param vars_dict: dict, Will be passed as Ansible extra-vars
@@ -65,24 +89,104 @@ def _run_playbook(cli_args, vars_dict):
     from ansible.cli.playbook import PlaybookCLI
     from ansible.errors import AnsibleOptionsError
     from ansible.errors import AnsibleParserError
-    with tempfile.NamedTemporaryFile(
-            mode='w+', prefix="ir-settings-", delete=True) as tmp:
-        tmp.write(yaml.safe_dump(vars_dict, default_flow_style=False))
-        # make sure created file is readable.
-        tmp.flush()
-        cli_args.extend(['--extra-vars', "@" + tmp.name])
-        cli = PlaybookCLI(cli_args)
-        LOG.debug('Starting ansible cli with args: {}'.format(cli_args[1:]))
+
+    rp, wp = os.pipe()
+    process_id = os.fork()
+
+    ansible_outputs_dir = os.path.join(ir_workspace.path, 'ansible_outputs')
+    try:
+        os.makedirs(ansible_outputs_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    # Child process
+    if not process_id:
+        os.close(wp)
+        fd_list = []
+
         try:
-            cli.parse()
-            # Return the result:
-            # 0: Success
-            # 1: "Error"
-            # 2: Host failed
-            # 3: Unreachable
-            # 4: Parser Error
-            # 5: Options error
-            return cli.run()
-        except (AnsibleParserError, AnsibleOptionsError) as error:
-            LOG.error('{}: {}'.format(type(error), error))
-            raise error
+            rp = os.fdopen(rp)
+
+            timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+            filename_template = "ir_{timestamp}_{plugin_name}{postfix}.log"
+
+            if not bool(strtobool(
+                    os.environ.get('IR_ANSIBLE_NO_STDOUT', 'no'))):
+                fd_list.append(sys.stdout)
+            if bool(strtobool(
+                    os.environ.get('IR_ANSIBLE_LOG_OUTPUT', 'no'))):
+                filename = filename_template.format(
+                    timestamp=timestamp, plugin_name=ir_plugin.name, postfix=''
+                )
+                log_file = os.path.join(ansible_outputs_dir, filename)
+                fd_list.append(open(log_file, 'w'))
+            if bool(strtobool(
+                    os.environ.get('IR_ANSIBLE_LOG_OUTPUT_NO_ANSI', 'no'))):
+                filename = filename_template.format(
+                    timestamp=timestamp, plugin_name=ir_plugin.name,
+                    postfix='_no_ansi'
+                )
+                log_file = os.path.join(ansible_outputs_dir, filename)
+                fd_list.append(NoAnsiFile(open(log_file, 'w')))
+
+            while True:
+                stdin_line = rp.readline()
+                if not stdin_line:
+                    break
+                for fd in fd_list:
+                    fd.write(stdin_line)
+                    fd.flush()
+
+        finally:
+            for fd in fd_list:
+                fd.close()
+            rp.close()
+
+        sys.exit(0)
+
+    # Parent process
+    else:
+        os.close(rp)
+        with tempfile.NamedTemporaryFile(
+                mode='w+', prefix="ir-settings-", delete=True) as tmp:
+            tmp.write(yaml.safe_dump(vars_dict, default_flow_style=False))
+            # make sure created file is readable.
+            tmp.flush()
+            cli_args.extend(['--extra-vars', "@" + tmp.name])
+            cli = PlaybookCLI(cli_args)
+            LOG.debug('Starting ansible cli with args: {}'.format(cli_args[1:]))
+            try:
+                cli.parse()
+
+                # Saves originals instead of using sys.__stdout__ &
+                # sys.__stderr__ in case these got changes at an earlier stage
+                org_stdout = sys.stdout
+                org_stderr = sys.stderr
+
+                wp = os.fdopen(wp, 'w')
+                sys.stdout = wp
+                if bool(strtobool(
+                        os.environ.get('IR_ANSIBLE_STDERR_TO_STDOUT', 'no'))):
+                    sys.stderr = wp
+
+                # Return the result:
+                # 0: Success
+                # 1: "Error"
+                # 2: Host failed
+                # 3: Unreachable
+                # 4: Parser Error
+                # 5: Options error
+                # sys.stderr.write('my error')
+                return cli.run()
+
+            except (AnsibleParserError, AnsibleOptionsError) as error:
+                LOG.error('{}: {}'.format(type(error), error))
+                raise error
+
+            finally:
+                sys.stdout.flush()
+                wp.close()
+
+                sys.stdout = org_stdout
+                sys.stderr = org_stderr
