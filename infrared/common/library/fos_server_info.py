32f9f15b25c9cc9daf5dc7dc98f7be9284563fc8
afazekas <afazekas@redhat.com>
2021-02-11 11:40:08 +0100

    going to remove the shady venvs
    ansible openstack using the 'openstacksdk/shade' library
    for accessing openstack. It is mainly problematic when
    the code has to run with old openstack.
    OSP13 end of life 2023.June.27 and is also incompatible.
    
    Infrared setting up various venvs for letting the new ansible
    working on older machines.
    However it is very fragile and even the containerised infrared
    cannot save you from random pip/pypi changes at
    remote venv creation.
    
    Moving all openstack call to python CLI clients,
    may work, but not fast.
    Most cases infrared is using the cli anyway.
    
    Another way to no need these venvs is creating our own openstack
    client which does not depends on more thing than cloud init.
    
    Note:
    Technically possible to create cleans just with curl/jq,
    it is also way faster then the `official` openstack-cli.
    
    This patch only includes the server listing module.
    
    This module also faster than then the original (0.5s),
    but other improvements might be possible in the future.
    
    Change-Id: Idbd9c3bbeca38f9eaf4a51c350f5155943ae86b0

---
+++ infrared/common/library/fos_server_info.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils import fos
+
+
+DOCUMENTATION = '''
+---
+module: fos_server_info
+short_description: Retrieve information about one or more compute instances
+description:
+    - Retrieve information about server instances from OpenStack.
+    - os_servers_facts module changing the openstack_servers facts
+notes:
+    - The result contains a list of servers.
+requirements:
+    - "python >= 2.7"
+options:
+   server:
+     description:
+       - restrict results to servers with names or UUID matching
+         this glob expression (e.g., <web*>).
+   detailed:
+     description:
+        - when true, return additional detail about servers at the expense
+          of additional API calls.
+     type: bool
+     default: 'no'
+   filters:
+     description:
+        - restrict results to servers matching a dictionary of
+          filters
+   availability_zone:
+     description:
+       - Ignored. Present for backwards compatibility
+   all_projects:
+     description:
+       - Whether to list servers from all projects or just the current auth
+         scoped project.
+     type: bool
+     default: 'no'
+extends_documentation_fragment: openstack
+'''
+
+EXAMPLES = '''
+# Gather information about all servers named <web*> that are in an active state:
+- fos_server_info:
+    cloud: rax-dfw
+    server: web*
+    filters:
+      vm_state: active
+  register: result
+- debug:
+    msg: "{{ result.openstack_servers }}"
+'''
+
+
+def is_facts_module(module):
+    return '_facts' in module._name
+
+
+def module_main(module, result):
+    params = module.params
+    os_client = fos.ClientContext(params)
+    openstack_servers = os_client.compute_server_search(
+        server=None if 'server' not in params else params['server'],
+        detailed=params['detailed'],
+        filters=params['filters'],
+        all_projects=params['all_projects'])
+    if is_facts_module(module):
+        result['ansible_facts'] = dict(
+            openstack_servers=openstack_servers)
+    else:
+        result["openstack_servers"] = openstack_servers
+
+
+def main():
+    # define available arguments/parameters a user can pass to the module
+    module_args = fos.openstack_full_argument_spec(
+        server=dict(required=False),
+        detailed=dict(required=False, type='bool', default=False),
+        filters=dict(required=False, type='dict', default=None),
+        all_projects=dict(required=False, type='bool', default=False),
+    )
+
+    result = dict(
+        changed=False,
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # module.check_mode
+    # This module allways in check mode, since it does not expected to change
+    # state.
+
+    try:
+        module_main(module, result)
+    except Exception as e:
+        module.fail_json(msg=str(e))
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
