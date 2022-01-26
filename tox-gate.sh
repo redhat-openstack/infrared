d7909a7c877d14b8a080508842be1e99f91d5a06
Pavel Sedlák <psedlak@redhat.com>
2022-01-27 00:44:59 +0100

    Require longer commit messages
    Lately there are issues with too brief commit messages
    for changes to InfraRed.
    
    Aside it being generally bad practice (especially for
    repository with many contributors, reviewers and users).
    
    There is also more practical reason why such commit messages are problematic.
    Recently (also in past but less) there were repeated
    cases when such commits introduced bugs, even in some critical CI areas,
    but not limited to that.
    
    When this happens, getting it fixed is very hard,
    as there is just not enough information what/where was broken
    before the change was introduced - and so anyone either doing revert
    or attempting to fixup the code, just cannot know if it's good enough,
    or what impact such action may have (can only know what was broken
    by the change, but not what it was fixing in first place).
    
    As such all proposed patches are required to provide more details
    about why they are needed and what they intend to affect (fix/improve...).
    But at same time, to not put this as extra work on reviewers or
    submitters, as also possibly provide earlier feedback,
    this initial crude check by CI can be performed to signal that the
    commit needs improvement.
    
    Change-Id: I6873a211c22b055f9cbbfec0a104b10a35fff523

--- tox-gate.sh
+++ tox-gate.sh
@@ -8,10 +8,38 @@ export TOXENV="pep8,py,cli,ansible-lint,any-errors-fatal,conflicts,plugin-regist
 
 python -m tox
 
+ANSI_RED="\033[01;31m"
+ANSI_NORMAL="\033[0m"
+
 if [[ ! -z $(git status -s) ]]; then
-    ANSI_RED="\033[01;31m"
-    ANSI_NORMAL="\033[0m"
     >&2 git status
     >&2 echo -e "${ANSI_RED}====== ^^ Failed: Modified files on disk, avoid them or update .gitignore file. ^^ ======${ANSI_NORMAL}"
+    exit 1
+fi
+
+# verify that there is CommitMessage Body provided
+# ideally reason why the change is done should be there
+# just linking to somewhere else is not good enough for commit history
+# while linking for more details is good, at least basic info should be here
+# (external refs do disappear/change over time too for ex.)
+# ... this is done as last here, so that any technical issues are pointed out first
+
+# list of what meta-keywords to exclude based on last 255 commits
+# when this was added, more to be added later based on practical experience
+commitMsgBody_excludeLines="Change-Id|Related|Related-Bug|Related-Change|Related-To|Resolves|Side-change|Signed-off-by|Solves|Tested-by|CIX|JIRA"
+commitMsgBody="$(git log --no-merges --pretty="%b" -1)"
+if ! grep -Ev "^\s*$|^(${commitMsgBody_excludeLines}):.*$" <<<"${commitMsgBody}"; then
+    (set +x;
+    >&2 echo -e "${ANSI_RED}====== ERROR: Commit message is too short. ======${ANSI_NORMAL}"
+    >&2 echo -e "${ANSI_RED}Commit message has to explain WHY the change is needed.${ANSI_NORMAL}"
+    >&2 echo "(e.g. what is broken or missing, why the feature is desired etc)"
+    >&2 echo ""
+    >&2 echo "Providing just single title-line, just reference to ticket or such meta info is not enough."
+    >&2 echo "Please write up little bit more details about the change."
+    >&2 echo ""
+    >&2 echo "If needed following are good guidelines about this:"
+    >&2 echo " - https://cbea.ms/git-commit/ (more general, esp. see the Seven Rules)"
+    >&2 echo " - https://wiki.openstack.org/wiki/GitCommitMessages (more technical, good/bad examples, external-references usually does not apply here)"
+    >&2 echo "")
     exit 1
 fi
