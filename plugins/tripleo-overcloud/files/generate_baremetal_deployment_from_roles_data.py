418576ef8b5634fb359f2d5148c40e1be882d705
Yogev Rabl <yrabl@redhat.com>
2022-05-06 10:55:13 -0400

    Enable HCI deployment case for OSP 17.0
    Adding a case for the profile relation between the baremetal_deployment.yaml
    and the profile of the nodes in instackenv.json.
    
    Adding 2 nic configuration files for the HCI node types.
    
    Change-Id: Ibd8bb9636923f30e67bd527b21a95834ad2ec659

--- plugins/tripleo-overcloud/files/generate_baremetal_deployment_from_roles_data.py
+++ plugins/tripleo-overcloud/files/generate_baremetal_deployment_from_roles_data.py
@@ -39,6 +39,8 @@ class BaremetalRoleData():
         # by default the "expanded" name,
         # but if a special tag is available use it
         # special cases
+        if 'hci' in self.name.lower():
+            return self.name.lower()
         for special_case in ['controller', 'compute']:
             if special_case in self._tags:
                 return special_case
@@ -144,7 +146,7 @@ def convert_role_data(roles_data: Dict, networks_data: Dict,
 
 def parse_opts(argv):
     parser = argparse.ArgumentParser(
-            description='Convert to Ansible Jinja2 NIC config templates.')
+        description='Convert to Ansible Jinja2 NIC config templates.')
     parser.add_argument('roles_data',
                         metavar='ROLES_DATA',
                         help='Path of the roles_data.yaml file',
