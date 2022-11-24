7e535bd580ea23bbe23bbedaf7129bd9d87eb578
Pavel Sedlák <psedlak@redhat.com>
2022-11-24 14:26:18 +0100

    Fix "conflicting subparser" for plugin import
    Newer python seems have issue that 'infrared plugin'
    action is trying to register 'import' action twice:
    
      Traceback (most recent call last):
        File "/tmp/compact-tox/infrared/.venv/bin/infrared", line 10, in <module>
          sys.exit(main())
                   ^^^^^^
        File "/tmp/compact-tox/infrared/infrared/main.py", line 527, in main
          specs_manager.register_spec(
        File "/tmp/compact-tox/infrared/infrared/api.py", line 196, in register_spec
          spec_object.extend_cli(self.root_subparsers)
        File "/tmp/compact-tox/infrared/infrared/main.py", line 342, in extend_cli
          import_parser = plugin_subparsers.add_parser(
                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
        File "/usr/lib64/python3.11/argparse.py", line 1185, in add_parser
          raise ArgumentError(self, _('conflicting subparser: %s') % name)
      argparse.ArgumentError: argument command0: conflicting subparser: import
    
    Based on the code, there are really two calls for:
    
      plugin_subparsers.add_parser('import'...
    
    following each other, so likely just forgotten copy paste (seems the
    code was not touched for years).
    
    Change-Id: Ib3a37de38f614138dbd3f934a584bdae7bd9fe1f

--- infrared/main.py
+++ infrared/main.py
@@ -335,10 +335,6 @@ class PluginManagerSpec(api.SpecObject):
             "rhos-infra organization on GitHub")
 
         # import plugins from registry yml file
-        plugin_subparsers.add_parser(
-            'import', help='Install plugins from a YAML file')
-
-        # Add plugin
         import_parser = plugin_subparsers.add_parser(
             'import', help='Install plugins from a registry YML file')
         import_parser.add_argument("src",
